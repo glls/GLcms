fbb499877f338dd902717fed67481bfc68f6e5f0
George Litos <gl@cyberpunk.gr>
2019-06-17 09:36:26 +0300

    reformat

--- themes/main/views/content/all.php
+++ themes/main/views/content/all.php
@@ -1,14 +1,14 @@
-<?php $this->widget('zii.widgets.CListView', array(
-    'dataProvider'=>$dataprovider,
-    'itemView'=>'//content/list',
-    'summaryText' => '',
-    'pager' => array(
-    	'htmlOptions' => array(
-    		'class' => 'pager'
-    	),
-    	'header' => '',
-    	'firstPageCssClass'=>'hide',
-    	'lastPageCssClass'=>'hide',
-    	'maxButtonCount' => 0
-    )
-));
+<?php $this->widget('zii.widgets.CListView', [
+    'dataProvider' => $dataprovider,
+    'itemView'     => '//content/list',
+    'summaryText'  => '',
+    'pager'        => [
+        'htmlOptions'       => [
+            'class' => 'pager'
+        ],
+        'header'            => '',
+        'firstPageCssClass' => 'hide',
+        'lastPageCssClass'  => 'hide',
+        'maxButtonCount'    => 0
+    ]
+]);

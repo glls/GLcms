6dd34bfc0348be1bd565134fd3f920a21a88683e
George Litos <gl@cyberpunk.gr>
2019-06-17 17:34:19 +0300

    Book impl

--- protected/controllers/BookController.php
+++ protected/controllers/BookController.php
@@ -79,11 +79,12 @@ class BookController extends Controller
 		));
 	}
 
-	/**
-	 * Updates a particular model.
-	 * If update is successful, the browser will be redirected to the 'view' page.
-	 * @param integer $id the ID of the model to be updated
-	 */
+    /**
+     * Updates a particular model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id the ID of the model to be updated
+     * @throws CHttpException
+     */
 	public function actionUpdate($id)
 	{
 		$model=$this->loadModel($id);
@@ -103,11 +104,13 @@ class BookController extends Controller
 		));
 	}
 
-	/**
-	 * Deletes a particular model.
-	 * If deletion is successful, the browser will be redirected to the 'admin' page.
-	 * @param integer $id the ID of the model to be deleted
-	 */
+    /**
+     * Deletes a particular model.
+     * If deletion is successful, the browser will be redirected to the 'admin' page.
+     * @param integer $id the ID of the model to be deleted
+     * @throws CDbException
+     * @throws CHttpException
+     */
 	public function actionDelete($id)
 	{
 		$this->loadModel($id)->delete();
